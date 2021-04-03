9ec946ac7741d9d96d7a5fd3a69af6f626759b0d
Devi Rukmana B <67756819+devirukmanab@users.noreply.github.com>
2021-04-04 03:49:59 +0700

    Add files via upload

---
+++ Javascript/challenge99BotolJus.js
@@ -0,0 +1,8 @@
+function botolJus() {
+	for (hitung = 99; hitung > 0; hitung--) {
+		console.log(hitung + " botol jus di kulkas, " + hitung + " botol jus di kulkas,");
+		console.log(" botol jus! Ambil satu, bagikan, " + hitung + " botol jus di kulkas,");  
+	}
+	console.log(hitung + " botol jus di kulkas " + hitung + " Tidak ada botol jus! Kami membawa mereka keluar, tidak ada lagi botol jus di kulkas!");
+}
+botolJus();
